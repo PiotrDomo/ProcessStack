f13fdedb4a5ef380325c6229bcea051daec85472
Piotr Domowicz (mindblower) <piotrdomowicz@gmail.com>
2019-02-21 11:01:05 +0100

    - MIT License added - Minor code changes

--- ProcessStack.swift
+++ ProcessStack.swift
@@ -1,16 +1,36 @@
-//
-//  ProcessStack.swift
-//  PatternAlert
-//
-//  Created by Piotr Domowicz on 23.01.19.
-//  Copyright © 2019 patternAlert.de. All rights reserved.
-//
+// ProcessStack.swift
+
+/*
+ 
+ MIT License
+ 
+ Copyright (c) 2018 ReusableScrollView (https://github.com/sumofighter666/ReusableScrollView)
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ 
+ */
 
 import Cocoa
 
 final class ProcessStack {
     
-    var tasks: Set = Set<Task>()
+    var processes: Set = Set<Process>()
     private var completion: (() -> Void)?
     
     // MARK: Public
@@ -20,16 +40,16 @@ final class ProcessStack {
         return self
     }
     
-    func addTask(task: Task) {
-        tasks.insert(task)
-        task.delegate = self
+    func addProcess(process: Process) {
+        processes.insert(process)
+        processes.delegate = self
     }
     
     func cancelAll() {
-        tasks.forEach{ task in
-            task.isCanceled = true
+        processes.forEach{ process in
+            process.isCanceled = true
         }
-        tasks = Set<Task>()
+        processes = Set<Process>()
     }
     
     // MARK: Private
@@ -39,11 +59,11 @@ final class ProcessStack {
     }
 }
 
-extension ProcessStack: TaskDelegate {
-    func didCompleteTask(task: Task) {
-        tasks.remove(task)
+extension ProcessStack: ProcessDelegate {
+    func didCompleteProcess(process: Process) {
+        processes.remove(process)
         
-        if tasks.isEmpty {
+        if processes.isEmpty {
             onCompletion()
         }
     }
